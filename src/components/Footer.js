c9a146a0c3a670f2e013e20c28a9dad9b61253d9
Mohit kumar <mohitpalk2728@gmail.com>
2020-11-12 23:00:18 +0530

    second commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Abc Pvt. Ltd.  Copyright ⓒ {year}</p>
+      <p>Abc Pvt. Ltd. Copyright ⓒ {year}</p>
     </footer>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,7 @@ import React from "react";
 function Header() {
   return (
     <header>
-      <h1>
-        
-        Employe List
-      </h1>
+      <h1>Employe List</h1>
     </header>
   );
 }
